4bb9df805ce50d8e26b02cbe08732d2915c4f1d0
Khushvant Jadhao <khusujadhao329@gmail.com>
2025-12-05 12:54:10 +0530

    Fixed ProtectedRoute import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './Components/ProtectedRoute';
 
 // Pages
 import Home from './Components/Home/Home';
